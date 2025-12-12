dea67564f02fa382e984da9e9f2bc7f218fbd5e0
Samson V <149072583+HappySamson@users.noreply.github.com>
2025-12-12 17:36:00 +0530

    Update Portfolio.js

--- src/Portfolio.js
+++ src/Portfolio.js
@@ -23,7 +23,6 @@ import ten from "./10th .jpg";
 import twelve from "./12th.jpg";
 import Swal from "sweetalert2";
 import emailjs from "emailjs-com";
-import "font-awesome/css/font-awesome.min.css";
 import { FaWhatsapp } from "react-icons/fa";
 import "sweetalert2/dist/sweetalert2.min.css";
 
